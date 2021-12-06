a937346817b71b7edd0ba49822bc387ead9cd696
Danilo Takata <daniloissamu@gmail.com>
2021-12-06 19:45:44 -0300

    Tentativa de subir no aws2

--- src/main/java/com/trabalhoFinal/perfectTrainer/controller/TreinoAcademiaController.java
+++ src/main/java/com/trabalhoFinal/perfectTrainer/controller/TreinoAcademiaController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/treinosAcademia")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class TreinoAcademiaController {
 
     @Autowired
